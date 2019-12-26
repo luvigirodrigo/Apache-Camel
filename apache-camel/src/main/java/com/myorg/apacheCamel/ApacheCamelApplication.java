7148882afa71bd2d0069b93eefb33d43f7a85629
Luvigi Priyanjana Rodrigo <luvigirodrigo6@gmail.com>
2019-12-26 23:25:57 +0530

    Commiting cinfigurations and properties

--- apache-camel/src/main/java/com/myorg/apacheCamel/ApacheCamelApplication.java
+++ apache-camel/src/main/java/com/myorg/apacheCamel/ApacheCamelApplication.java
@@ -1,13 +1,36 @@
 package com.myorg.apacheCamel;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.myorg.apacheCamel.example_1.CamelHelloWorld;
+import com.myorg.apacheCamel.example_1.CamelHelloWorldRouteBuilder;
+
 @SpringBootApplication
-public class ApacheCamelApplication {
+public class ApacheCamelApplication implements CommandLineRunner {
+
+	@Autowired
+	@Qualifier("camelHelloWorld")
+	public CamelHelloWorld camelHelloWorld;
+
+	@Autowired
+	@Qualifier("camelHelloWorldRouteBuilder")
+	CamelHelloWorldRouteBuilder camelHelloWorldRouteBuilder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApacheCamelApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// Camel - Hello World example
+		executeCamelExample1();
+	}
+
+	public void executeCamelExample1() {
+		camelHelloWorld.executeHelloWorld(camelHelloWorldRouteBuilder);
+	}
 }
--- apache-camel/src/main/java/com/myorg/apacheCamel/configuration/CamelConfiguration.java
+++ apache-camel/src/main/java/com/myorg/apacheCamel/configuration/CamelConfiguration.java
@@ -1,5 +1,20 @@
 package com.myorg.apacheCamel.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.myorg.apacheCamel.example_1.CamelHelloWorld;
+import com.myorg.apacheCamel.example_1.CamelHelloWorldRouteBuilder;
+
+@Configuration
 public class CamelConfiguration {
-	
+	@Bean(name = "camelHelloWorld")
+	public CamelHelloWorld camelHelloWorld() {
+		return new CamelHelloWorld();
+	}
+
+	@Bean(name = "camelHelloWorldRouteBuilder")
+	public CamelHelloWorldRouteBuilder camelHelloWorldRouteBuilder() {
+		return new CamelHelloWorldRouteBuilder();
+	}
 }
