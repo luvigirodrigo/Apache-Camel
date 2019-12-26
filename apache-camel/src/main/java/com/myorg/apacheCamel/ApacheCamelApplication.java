9db5994aac9fe6225a7957644821c3aa91ddf4d9
Luvigi Priyanjana Rodrigo <luvigirodrigo6@gmail.com>
2019-12-26 19:54:20 +0530

    Commiting project structure and relevant files

---
+++ apache-camel/src/main/java/com/myorg/apacheCamel/ApacheCamelApplication.java
@@ -0,0 +1,13 @@
+package com.myorg.apacheCamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelApplication.class, args);
+	}
+
+}
---
+++ apache-camel/src/test/java/com/myorg/apacheCamel/ApacheCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorg.apacheCamel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
